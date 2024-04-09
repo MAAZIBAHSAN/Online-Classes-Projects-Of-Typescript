99bd668716a6e301955dd496adc115af8c664c71
M.AAZIBAHSAN <160249107+MAAZIBAHSAN@users.noreply.github.com>
2024-04-09 17:29:41 +0500

    MY FIRST COMMIT

---
+++ CLASS 02 SIMPLE CALCULATOR/SimpleCalculator.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node  
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "ENTER FIRST NUMBER", type: "number", name: "firstnumber" },
+    { message: "ENTER SECOND NUMBER", type: "number", name: "secondnumber" },
+    {
+        message: "Select one of the valid operators to perform action",
+        type: "list",
+        name: "Operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+// conditional statment
+if (answer.Operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.Operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.Operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.Operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select a valid Operator");
+}
