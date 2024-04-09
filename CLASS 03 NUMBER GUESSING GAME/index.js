7dc493bdc44ced13352d7d863b4c4e38b879e398
M.AAZIBAHSAN <160249107+MAAZIBAHSAN@users.noreply.github.com>
2024-04-09 17:24:49 +0500

    MY FIRST COMMIT

---
+++ CLASS 03 NUMBER GUESSING GAME/index.js
@@ -0,0 +1,21 @@
+#! usr/env/bin node
+import inquirer from "inquirer";
+// 1) compiler will generate a random number - Done.
+// 2) user input for guesing number - Done.
+// 3) compare user input with computer generated  number and show result Done.
+const randomNumber = Math.floor(Math.random() * 6 + 1); // generates a random number between
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1 - 6: ",
+    },
+]);
+console.log(answers);
+if (answers.UserGuessedNumber === randomNumber) {
+    console.log("congratulations! you guessed right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
+
