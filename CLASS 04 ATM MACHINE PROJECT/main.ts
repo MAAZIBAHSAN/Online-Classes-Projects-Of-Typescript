37b263fee0a5d39cd3eca8df7103c054321cf43f
M.AAZIBAHSAN <160249107+MAAZIBAHSAN@users.noreply.github.com>
2024-04-15 16:09:46 +0500

    MY FIRST COMMIT

---
+++ CLASS 04 ATM MACHINE PROJECT/main.ts
@@ -0,0 +1,60 @@
+import inquirer from "inquirer";
+
+let MyBalance = 10000; // DOLLAR
+
+let MyPin = 1234; // PIN
+
+let pinAnswer = await inquirer.prompt(
+   
+    [
+        {
+            name: 'pin',
+            message: 'enter your pin number',
+            type: 'number'
+        } 
+
+    ]
+);
+
+// 12345 === 1234 - false
+
+if (pinAnswer.pin === MyPin) {
+    console.log("  Correct Pin Code!!!");
+ 
+    let operationAns = await inquirer.prompt(
+        [
+            {
+                name:"operation",
+                message:"please select one option",
+                type:"list",
+                choices: ["withdraw" , "check balance"]
+            }
+        ]
+    );
+     console.log(operationAns);
+     
+
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name:"amount",
+                    message:"enter your amount",
+                    type:"number"
+                }
+            ]
+        );
+        // =, -=, += 
+        MyBalance -= amountAns.amount;
+
+        console.log("Your Remaining Balance Is: "  + MyBalance);
+
+    } else if (operationAns.operation === "check balance"){
+        console.log("Your Balance Is: " + MyBalance)
+    }
+
+}
+
+else  {
+    console.log("Incorrect Pin Number");  
+} 
